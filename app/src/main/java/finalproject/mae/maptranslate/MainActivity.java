ff2dfe04224e00b4f69efe744f7a36c45d597cc0
Akash <khalkash@gmail.com>
2017-11-30 12:40:14 -0500

    Initial Commit

---
+++ app/src/main/java/finalproject/mae/maptranslate/MainActivity.java
@@ -0,0 +1,16 @@
+package finalproject.mae.maptranslate;
+/**
+ * Final Project for Mobile App Engineering
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
