fd2cf038b07dbb1bec633c7d86348d736ce62f31
Akash <khalkash@gmail.com>
2017-12-04 22:48:35 -0500

    Issue with translation. Not sure which method to invoke. Can get source language.

--- app/src/main/java/finalproject/mae/maptranslate/ImageTranslation/TranslationActivity.java
+++ app/src/main/java/finalproject/mae/maptranslate/ImageTranslation/TranslationActivity.java
@@ -1,6 +1,8 @@
 package finalproject.mae.maptranslate.ImageTranslation;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,14 +16,20 @@ import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 import com.google.cloud.translate.Detection;
+import com.google.cloud.translate.Language;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
 
+import java.util.Iterator;
+import java.util.List;
+
+import finalproject.mae.maptranslate.MainActivity;
 import finalproject.mae.maptranslate.R;
 
 public class TranslationActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String API_KEY = "AIzaSyDyDuLRbiaqxZgpX0yy-03zNoZXTCndi54";
+    String targetLanguage;
     Button goodButton;
     Button badButton;
     TextView origin;
@@ -31,6 +39,7 @@ public class TranslationActivity extends AppCompatActivity implements View.OnCli
     String extractedText;
     String translatedText;
     TextView detectText;
+    String language;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +54,8 @@ public class TranslationActivity extends AppCompatActivity implements View.OnCli
         image.setImageBitmap(originalImage);
         extractedText = "Original Text: ";
         translatedText = "Translated Text: ";
+        targetLanguage = "de";
+        language = "";
         extractTextFromImage();
 
     }
@@ -68,12 +79,13 @@ public class TranslationActivity extends AppCompatActivity implements View.OnCli
     }
 
     public void translateText(){
-    TranslateOptions options = TranslateOptions.newBuilder().setApiKey(API_KEY).build();
-    Translate translate = options.getService();
-    Detection detector = translate.detect(extractedText);
-    detectText.setText(detector.getLanguage());
-    /*Translation translationString = translate.translate(extractedText);
-    translation.setText(translationString.getTranslatedText());*/
+     Log.d("translateText", "before Async text");
+     //LAUNCHES TWO ASYNC TASKS TO GET TRANSLATION
+        //language task
+     AsyncTask newTask = new LanguageTask().execute(extractedText);
+        //translation task
+     new TranslateTask().execute(extractedText);
+
     }
 
 
@@ -81,13 +93,67 @@ public class TranslationActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View v){
         if(v.getId() == R.id.badbutt){
             //return to map activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
 
         }
         else if(v.getId() == R.id.goodbutt){
             //add to database
 
+
+            //return to map activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
         }
 
+
+    }
+
+    //Language Asynchronous Task that gets the language of the extracted text
+    class LanguageTask extends AsyncTask<String, String, String>{
+        private Exception exception;
+        protected String doInBackground(String...originalText){
+            Log.d("In AsyncTask", "LanguageTask doInBackground");
+            Detection detector = null;
+            try{
+                TranslateOptions options = TranslateOptions.newBuilder().setApiKey(API_KEY).build();
+                Translate translate = options.getService();
+                detector = translate.detect(extractedText);
+
+            }
+            catch(Exception e){
+                this.exception = e;
+                return null;
+            }
+
+            return detector.getLanguage();
+        }
+        protected void onPostExecute(String res){
+            detectText.setText("Language: " +  res);
+        }
+
+    }
+    //Translate Asynchronous Task that gets the translation of the source text
+    class TranslateTask extends AsyncTask<String, String, String>{
+        protected String doInBackground(String...originalText){
+            Detection detector = null;
+            try{
+                //Not sure what to do here, methods that translate seem to not exist?
+                TranslateOptions options = TranslateOptions.newBuilder().setApiKey(API_KEY).build();
+                TranslateOptions targetLang = TranslateOptions.newBuilder().setTargetLanguage(targetLanguage).build();
+                Translate translate = options.getService();
+                translate.translate(extractedText, targetLang); //????????????
+
+            }
+            catch(Exception e){
+                return null;
+            }
+            return null;
+        }
+        protected void onPostExecute(String res){
+
+        }
 
     }
 }
